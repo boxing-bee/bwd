aec9a638314007e94160912219fc0735b46ede5b
boxing-bee <benishk415@gmail.com>
2023-11-08 22:58:28 -0800

    update cypress test take 2

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -7,11 +7,5 @@ describe('empty spec', () => {
     cy.get('p')
     .contains('I had a great time working out with you!');
   })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+  
   })
-})
